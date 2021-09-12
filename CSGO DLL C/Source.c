2a915621ff37086377e24c7a14417026f4d5d760
Rémi Hage <remihage2003@gmail.com>
2021-09-12 10:26:56 +0200

    Fixed code and updated offsets

--- CSGO DLL C/Source.c
+++ CSGO DLL C/Source.c
@@ -3,13 +3,20 @@
 #include <stdbool.h>
 #include <Windows.h>
 
-const int dwForceJump = 0x524F21C;
+const int dwForceJump = 0x524F20C;
+const int dwForceAttack = 0x31D59A4;
 const int dwLocalPlayer = 0xD8C2CC;
+const int dwEntityList = 0x4DA541C;
 const int m_lifeState = 0x25F;
 const int m_fFlags = 0x104;
 const int m_bSpotted = 0x93D;
-const int dwEntityList = 0x4DA542C;
+const int m_bDormant = 0xED;
+const int m_iTeamNum = 0xF4;
+const int m_iCrosshairId = 0xB3E8;
+
 #define FL_ONGROUND (1 << 0)
+
+int client, localPlayer;
 
 enum LifeState
 {
@@ -20,27 +27,71 @@ enum LifeState
 	LIFE_DISCARDBODY
 };
 
+void Bunnyhop(bool enable)
+{
+	if (enable == true)
+	{
+		BYTE flags = *(BYTE*)(localPlayer + m_fFlags);
+
+		if (GetAsyncKeyState(VK_SPACE) && flags & FL_ONGROUND)
+			*(int*)(client + dwForceJump) = 6;
+	}
+}
+
+void Radarhack(bool enable)
+{
+	if (enable == true)
+	{
+		for (int i = 1; i < 64; i++) // Looping through all entities
+		{
+			int ent = *(int*)((client + dwEntityList) + i * 0x10); // Accessing each one of them
+			if (ent) *(bool*)(ent + m_bSpotted) = 1;
+		}
+	}
+}
+
+void Triggerbot(bool enable, int vKey)
+{
+	if (enable == true)
+	{
+		int crossId = *(int*)(localPlayer + m_iCrosshairId);
+		int myTeam = *(int*)(localPlayer + m_iTeamNum);
+
+		if (crossId && crossId < 64)
+		{
+			int ent = *(int*)((client + dwEntityList) + (crossId - 1) * 0x10);
+
+			if (ent && !*(bool*)(ent + m_bDormant))
+			{
+				int entTeam = *(int*)(ent + m_iTeamNum);
+
+				if (entTeam != myTeam)
+					if (GetAsyncKeyState(vKey))
+						*(int*)(client + dwForceAttack) = 6;
+			}
+		}
+	}
+}
+
 int Main(HMODULE hMod)
 {
-	int client = (int)GetModuleHandle("client.dll");
+	// Grabbing client.dll base address
+	client = (int)GetModuleHandle("client.dll");
 
 	while (!GetAsyncKeyState(VK_END))
 	{
-		int localPlayer = *(int*)(client + dwLocalPlayer);
-		int playerState = *(int*)(localPlayer + m_lifeState);
+		// Reading memory in real time
+		localPlayer = *(int*)(client + dwLocalPlayer);
 
-		BYTE flags = *(BYTE*)(localPlayer + m_fFlags);					// Bunnyhop
-
-		if (playerState == LIFE_ALIVE)
-			if (GetAsyncKeyState(VK_SPACE) && flags & FL_ONGROUND)
-				*(int*)(client + dwForceJump) = 6;
-
-		for (int i = 1; i < 64; i++)									// Radarhack
+		// Calling our functions/features
+		if (localPlayer && *(int*)(localPlayer + m_lifeState) == LIFE_ALIVE)
 		{
-			int ent = *(int*)((client + dwEntityList) + i * 0x10);
-			if (ent != NULL) *(bool*)(ent + m_bSpotted) = 1;
+			Bunnyhop(true);
+			Radarhack(true);
+			Triggerbot(true, 'A');
 		}
 
+		// Sleep to save some cpu
 		Sleep(5);
 	}
 
